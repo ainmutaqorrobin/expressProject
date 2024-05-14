3bd431c3caa808d79573d4cdb705735427352677
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-05-14 11:09:55 +0800

    added userController and tourController

---
+++ controllers/tourController.js
@@ -0,0 +1,81 @@
+const fs = require('fs');
+
+//read tours from local data in json format
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+//method for get all tours
+exports.getAllTours = (request, respond) => {
+  console.log(request.requestTime);
+  respond.status(200).json({
+    status: 'success',
+    requestTime: request.requestTime,
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+};
+
+//method for get single tour
+exports.getSingleTour = (request, respond) => {
+  const tour = tours.find((el) => el.id === +request.params.id);
+  if (!tour) {
+    return respond
+      .status(404)
+      .json({ status: 'Failed', message: 'Failed to search the tour' });
+  }
+  respond.status(200).json({ status: 'Success', data: { tours: tour } });
+};
+
+//method for create new tour
+exports.createTour = (request, respond) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, request.body); //create new object from request
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    () => {
+      respond.status(201).json({
+        status: 'successfully add new tour',
+        data: {
+          tours: tours,
+        },
+      });
+    }
+  );
+};
+
+//method for update tour
+exports.updateTour = (request, respond) => {
+  const tour = tours.find((el) => el.id === +request.params.id);
+  if (!tour) {
+    return respond.status(404).json({
+      status: 'Failed',
+      message: 'Could not find and update the tour',
+    });
+  }
+  respond.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  });
+};
+
+//method for delete tour
+exports.deleteTour = (request, respond) => {
+  const tour = tours.find((el) => el.id === +request.params.id);
+  if (!tour) {
+    return respond.status(404).json({
+      status: 'Failed',
+      message: 'Failed to delete the selected tour',
+    });
+  }
+  respond.status(204).json({
+    status: 'Success',
+    data: null,
+  });
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,36 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
-const getAllUsers = (request, respond) => {
-  respond.status(500).json({
-    status: 'Error',
-    message: 'This route is not yet implemented.',
-  });
-};
-const getSingleUser = (request, respond) => {
-  respond.status(500).json({
-    status: 'Error',
-    message: 'This route is not yet implemented.',
-  });
-};
-const createUser = (request, respond) => {
-  respond.status(500).json({
-    status: 'Error',
-    message: 'This route is not yet implemented.',
-  });
-};
-const updateUser = (request, respond) => {
-  respond.status(500).json({
-    status: 'Error',
-    message: 'This route is not yet implemented.',
-  });
-};
-const deleteUser = (request, respond) => {
-  respond.status(500).json({
-    status: 'Error',
-    message: 'This route is not yet implemented.',
-  });
-};
+const {
+  getAllUsers,
+  createUser,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/userController');
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getSingleUser).patch(updateUser).delete(deleteUser);
