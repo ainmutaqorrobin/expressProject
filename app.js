941dd6aed5506c96e10d7c9bf51bb525ec3c4510
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-05-16 18:14:27 +0800

    added environment file

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
-
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const app = express();
+
 //using middleware
-app.use(morgan('dev '));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+console.log(process.env.NODE_ENV);
 app.use(express.json());
-app.use((request, respond, next) => {
-  console.log(`wassap geng its robin`);
-  next();
-});
+app.use(express.static(`${__dirname}/public`));
 
 app.use((request, respond, next) => {
   request.requestTime = new Date().toISOString();
