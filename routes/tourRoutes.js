e957315044138de96facb09e7c7bf760c04c77c0
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-05-24 08:27:21 +0800

    using pipeline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,10 +7,13 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
+  getMonthlyStats,
 } = require('../controllers/tourController');
 
 // router.param('id', validateID);
-
+router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-stats/:year').get(getMonthlyStats);
 router.route('/top-5-tours').get(aliasTopTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getSingleTour).patch(updateTour).delete(deleteTour);
