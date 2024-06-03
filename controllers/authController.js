877b4cb9450d8359bdc15ba29f7f84a0163631a9
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-06-03 22:59:11 +0800

    using JWT for signup user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,22 @@
 const User = require('../models/userModel');
 const catchAsyncError = require('../utils/catchAsyncError');
+const jwt = require('jsonwebtoken');
 
 exports.signUp = catchAsyncError(async (request, respond, next) => {
-  const newUser = await User.create(request.body);
+  const newUser = await User.create({
+    name: request.body.name,
+    email: request.body.email,
+    password: request.body.password,
+    passwordConfirm: request.body.passwordConfirm,
+  });
 
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   respond.status(201).json({
     status: 'Successful.',
     message: 'Created new user.',
+    token: token,
     data: {
       user: newUser,
     },
