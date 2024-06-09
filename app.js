7d92a89d40faaaa7197727df612dbdff4a140b8b
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-06-10 00:04:21 +0800

    data sanitization agains XSS and noSQL query injection

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const app = express();
 
 //to make HTTP Header secured
@@ -27,6 +28,14 @@ app.use('/api', limiter);
 
 //body parser, reading data from body into request body
 app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss())
+
+//read static file
 app.use(express.static(`${__dirname}/public`));
 
 //test middleware
