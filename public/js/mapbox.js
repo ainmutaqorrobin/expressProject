4e75278720b3a3f8a07af5fbfe0fb09948908031
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-06-25 17:13:58 +0800

    used mapbox (continue later)

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,3 +1,10 @@
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 
-console.log(locations);
+mapboxgl.accessToken = '<your access token here>';
+
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/mapbox/streets-v12', // style URL
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9, // starting zoom
+});
