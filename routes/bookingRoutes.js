cae742b40064f12dcf7e3e961ce466b2b2da3d77
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-07-16 05:42:11 +0800

    booking API route and handler

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,10 +4,20 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.get(
-  '/checkout-session/:tourId',
-  authController.checkAuthentication,
-  bookingController.getCheckoutSession
-);
+router.use(authController.checkAuthentication);
+
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+router
+  .route('/:id')
+  .get(bookingController.getSingleBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.createBooking);
 
 module.exports = router;
